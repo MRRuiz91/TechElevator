2e90216bb92bc92e3f871ed19757bb64c9da656e
Zachary Wilson <zlwilson21@gmail.com>
2020-12-04 12:54:47 -0500

    Added integration testing framework

---
+++ final-capstone/dotnet/Capstone/Tests/AnimalShelterCapstonTests/AnimalShelterCapstonTests/DAL/AnimalDAOTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
+
+namespace CapstoneTests.DAL
+{
+    [TestClass]
+    class AnimalDAOTests
+    {
+        protected string ConnectionString { get; } = "Server=.\\SQLEXPRESS;Database=Capstone;Trusted_Connection=True;";
+
+        protected int NewAnimalID { get; private set; }
+
+        private TransactionScope transaction;
+
+
+        public void Setup()
+        {
+            // Begin the transaction
+            transaction = new TransactionScope();
+
+            // Get the SQL Script to run
+            string sql = File.ReadAllText("test-script.sql");
+
+            // Execute the script
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // If there is a row to read
+                if (reader.Read())
+                {
+                    this.NewAnimalID = Convert.ToInt32(reader["pet_id"]);
+                }
+            }
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Roll back the transaction
+            transaction.Dispose();
+
+        }
+
+        /// <summary>
+        /// Gets the row count for a table.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        protected int GetRowCount(string table)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT COUNT(*) FROM {table}", conn);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count;
+            }
+        }
+    }
+}
---
+++ final-capstone/dotnet/database/test-script.sql
@@ -0,0 +1,63 @@
+USE master
+GO
+
+--drop database if it exists
+IF DB_ID('final_capstone') IS NOT NULL
+BEGIN
+	ALTER DATABASE final_capstone SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE final_capstone;
+END
+
+CREATE DATABASE final_capstone
+GO
+
+USE final_capstone
+GO
+
+--create tables
+CREATE TABLE users (
+	user_id int IDENTITY(1,1) NOT NULL,
+	username varchar(50) NOT NULL,
+	password_hash varchar(200) NOT NULL,
+	salt varchar(200) NOT NULL,
+	user_role varchar(50) NOT NULL
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE pets (
+	pet_id int IDENTITY(1,1) NOT NULL,
+	pet_type varchar(64) NOT NULL,
+	pet_age int,
+	pet_name varchar(64) NOT NULL,
+	pet_image varchar(200),
+	is_adopted bit NOT NULL,
+	arrival_date dateTime NOT NULL,
+	adoption_date dateTime,
+	adopted_by varchar(64)
+	CONSTRAINT PK_pet PRIMARY KEY (pet_id)
+	
+);
+/*
+CREATE TABLE species (
+	species_id int IDENTITY(1,1) NOT NULL,
+	species_name varchar(64) NOT NULL,
+	CONSTRAINT PK_breed PRIMARY KEY (speciesId)
+);
+
+CREATE TABLE breeds (
+	breed_id int IDENTITY(1,1) NOT NULL,
+	breed_name varchar(64) NOT NULL,
+	CONSTRAINT PK_breed PRIMARY KEY (breed_id)
+);
+
+AlTER TABLE pets 
+ADD FOREIGN KEY(pet_type) REFERENCES breed(breed_id)
+*/
+
+
+--populate default data
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+INSERT INTO pets (pet_type, pet_age, pet_name, pet_image, is_adopted, arrival_date, adoption_date, adopted_by) VALUES ('Elephant', 49, 'Mai-Thai', 'Image goes here', 0, 12/04/2020, 'N/A', 'N/A')
+GO
