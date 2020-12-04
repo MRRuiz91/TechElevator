117e5c414e9d650c07637dd8b865f19bcedd38c9
firstname lastname <matt.ruizcg@yahoo.com>
2020-12-04 12:06:33 -0500

    pushing with start database and tables

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -22,7 +22,32 @@ CREATE TABLE users (
 	salt varchar(200) NOT NULL,
 	user_role varchar(50) NOT NULL
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-)
+);
+
+CREATE TABLE pets (
+	petId int IDENTITY(1,1) NOT NULL,
+	petType varchar(64) NOT NULL,
+	petAge int,
+	petName varchar(64) NOT NULL,
+	petImage varchar(200),
+	isAdopted bit NOT NULL,
+	arrivalDate dateTime NOT NULL,
+	adoptionDate dateTime,
+	adoptedBy varchar(64)
+	CONSTRAINT PK_pet PRIMARY KEY (petId)
+	
+);
+
+CREATE TABLE breed (
+	breedId varchar(64) NOT NULL,
+	breedName varchar(64) NOT NULL
+	CONSTRAINT PK_breed PRIMARY KEY (breedId)
+);
+
+AlTER TABLE pets 
+ADD FOREIGN KEY(petType) REFERENCES breed(breedId)
+
+
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
