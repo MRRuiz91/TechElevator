b8266e9d3d2aa30d98b670d80f31d7351ea8ad32
Zachary Wilson <zlwilson21@gmail.com>
2020-12-07 11:30:59 -0500

    Fiddling with the PetsService and the Get request

--- final-capstone/vue/src/components/BrowsePets.vue
+++ final-capstone/vue/src/components/BrowsePets.vue
@@ -1,11 +1,16 @@
 <template>
-
+    <div>
+        <h1>{{pet.Name}}</h1>
+        <img src="{{pet.Picture}}" alt="image not found."/>
+    </div>
   
 </template>
 
 <script>
-export default {
+import PetsService from '@/services/PetsService.js';
 
+export default {
+    props: ['pet']
 }
 </script>
 
