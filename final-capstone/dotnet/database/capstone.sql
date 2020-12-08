613097c66aa930785b751bfc6b53e3663b4cdef7
Zachary Wilson <zlwilson21@gmail.com>
2020-12-08 17:47:07 -0500

    making sure sql saved

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -61,34 +61,35 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg4
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date,adoption_date, adopted_by)
-VALUES (3, 'Roger', 'https://flic.kr/p/2kexmF4', 1, 07/08/2019, 12/08/2020, 'Jason Picardi');
+VALUES (3, 'Roger', 'https://live.staticflickr.com/65535/50692703601_19cbf11613_c.jpg', 1, 07/08/2019, 12/08/2020, 'Jason Picardi');
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (2, 'Fred', 'https://flic.kr/p/2ketBmG', 0, 02/07/2019);
+VALUES (2, 'Fred', 'https://live.staticflickr.com/65535/50691972548_bc527a54b0_c.jpg
+', 0, 02/07/2019);
 
 INSERT INTO pets (pet_name, pet_image, is_adopted, arrival_date)
-VALUES ('Hank and Dale', 'https://flic.kr/p/2kexqTT', 0, 06/02/2019);
+VALUES ('Hank and Dale', 'https://live.staticflickr.com/65535/50692717801_554dfb5aa3_c.jpg', 0, 06/02/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (8, 'Grace', 'https://flic.kr/p/2kexKLt', 0, 06/08/2019);
+VALUES (8, 'Grace', 'https://live.staticflickr.com/65535/50692781287_74ab776e1b_c.jpg', 0, 06/08/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (3, 'Frida', 'https://flic.kr/p/2kexmFj', 0, 12/08/2019);
+VALUES (3, 'Frida', 'https://live.staticflickr.com/65535/50692703616_d59ec26f99_c.jpg', 0, 12/08/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (3, 'Juniper', 'https://flic.kr/p/2kexKJQ', 0, 09/09/2019);
+VALUES (3, 'Juniper', 'https://live.staticflickr.com/65535/50692781192_1b59149184_c.jpg', 0, 09/09/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (6, 'Willow', 'https://flic.kr/p/2keKNqY', 0, 10/10/2019);
+VALUES (6, 'Willow', 'https://live.staticflickr.com/65535/50695131592_2c99144b3e_c.jpg', 0, 10/10/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (2, 'Simone', 'https://flic.kr/p/2keFyEA', 0, 10/20/2019);
+VALUES (2, 'Simone', 'https://live.staticflickr.com/65535/50694304838_ca9d408eb1_c.jpg', 0, 10/20/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (5, 'Bear', 'https://flic.kr/p/2keFyEL', 0, 10/20/2019);
+VALUES (5, 'Bear', 'https://live.staticflickr.com/65535/50694304848_d1924368fa_c.jpg', 0, 10/20/2019);
 
 INSERT INTO pets (pet_age, pet_name, pet_image, is_adopted, arrival_date)
-VALUES (2, 'George', 'https://flic.kr/p/2key7KH', 0, 12/08/2020);
+VALUES (2, 'George', 'https://live.staticflickr.com/65535/50692851887_e9a7a1586e_c.jpg', 0, 12/08/2020);
 
 INSERT INTO pets (pet_age, pet_name, is_adopted, arrival_date)
 VALUES (3, 'Arlo', 0, 01/01/2020);
